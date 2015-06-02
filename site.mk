c110c9e7b6fa2af02016b510e0a99514a8028014
stebifan <stefan@tro-mail.de>
2015-06-02 11:28:45 +0200

    Removed Netwatch Script

--- site.mk
+++ site.mk
@@ -25,8 +25,7 @@ GLUON_SITE_PACKAGES := \
     gluon-status-page \
     iwinfo \
     iptables \
-    haveged \
-    tro-netwatch
+    haveged
 
 DEFAULT_GLUON_RELEASE := 2014.4-stable-2.1
 # Allow overriding the release number from the command line
