ea2bdb93f6b2a332b281ab2c3be83ed77a28f295
stebifan <stefan@tro-mail.de>
2015-09-22 20:22:27 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -23,17 +23,16 @@ GLUON_SITE_PACKAGES := \
     gluon-mesh-vpn-fastd \
     gluon-radvd \
     gluon-status-page \
-    gluon-authorized-keys \
     iwinfo \
     iptables \
     haveged \
     tro-netwatch \
     tro-offline_ssid \
     kmod-usb-core \
-	kmod-usb2 \
-	kmod-usb-hid \
-	kmod-usb-net \
-	kmod-usb-net-asix
+    kmod-usb2 \
+    kmod-usb-hid \
+    kmod-usb-net \
+    kmod-usb-net-asix
 
 DEFAULT_GLUON_RELEASE := 2014.4-stable-2.1
 # Allow overriding the release number from the command line
