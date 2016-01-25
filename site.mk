9e7309e1382d83efcc9c17e09d4f9974c7d7171a
stebifan <stefan@tro-mail.de>
2016-01-25 15:44:52 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -21,6 +21,7 @@ GLUON_SITE_PACKAGES := \
     gluon-luci-node-role \
     gluon-next-node \
     gluon-mesh-vpn-tunneldigger \
+    gluon-tunneldigger-watchdog \
     gluon-radvd \
     gluon-status-page \
     iwinfo \
