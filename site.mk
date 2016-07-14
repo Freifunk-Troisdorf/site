c68869a4394d9d7ed2aff4aca647c176b77a1bfe
stebifan <stefan@tro-mail.de>
2016-07-14 19:11:44 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -29,8 +29,7 @@ GLUON_SITE_PACKAGES := \
     tro-netwatch \
     tro-nightboot \
     gluon-ssid-changer \
-    gluon-banner \
-    gluon-airtime
+    gluon-banner
 
 DEFAULT_GLUON_RELEASE := 2014.4-stable-2.1
 # Allow overriding the release number from the command line
